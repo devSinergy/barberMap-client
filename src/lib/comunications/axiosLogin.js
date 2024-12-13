25387231c833682f7613905595994f353d8bdb99
eduardof5sg <149663359+eduardof5sg@users.noreply.github.com>
2024-12-14 00:40:17 +0100

    frontend_prueba de cookies 002

--- src/lib/comunications/axiosLogin.js
+++ src/lib/comunications/axiosLogin.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const apiLogin = axios.create({
-    baseURL:'https://barbermap-server.onrender.com/auth/login',
+    baseURL:'https://barbermap-server.onrender.com/auth',
     timeout:5000,
     headers:{
         'Content-Type':'application/json'
